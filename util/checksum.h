72bd5f2df2af7e7cc03e72147b96133ee71f956a
SlimTim10 <slimtim10@gmail.com>
2020-04-13 12:20:58 -0400

    Implement checksum helpers

--- util/checksum.h
+++ util/checksum.h
@@ -3,18 +3,52 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
-uint16_t fletcher16(uint8_t *data, size_t count) {
+uint16_t fletcher16(uint8_t *data, size_t length) {
 	uint16_t sum1 = 0;
 	uint16_t sum2 = 0;
 	size_t i;
 
-	for (i = 0; i < count; i++) {
+	for (i = 0; i < length; i++) {
 		sum1 = (sum1 + data[i]) % 255;
 		sum2 = (sum2 + sum1) % 255;
 	}
 
 	return (sum2 << 8) | sum1;
+}
+
+bool check(uint8_t *data, size_t length) {
+	return fletcher16(data, length) == 0x0000;
+}
+
+/* The size of the dst buffer must be at least 2 bytes larger than srcLength */
+uint8_t *withCheckBytes(uint8_t *src, uint8_t *dst, size_t srcLength) {
+	uint16_t csum;
+	uint8_t c0, c1, f0, f1;
+
+	csum = fletcher16(src, srcLength);
+	f0 = csum & 0xFF;
+	f1 = (csum >> 8) & 0xFF;
+	c0 = 0xFF - ((f0 + f1) % 0xFF);
+	c1 = 0xFF - ((f0 + c0) % 0xFF);
+
+	memcpy(dst, src, srcLength);
+	dst[srcLength] = c0;
+	dst[srcLength + 1] = c1;
+	
+	return dst;
+}
+
+/* The size of the dst buffer must be at least 2 bytes less than srcLength */
+uint8_t *withoutCheckBytes(uint8_t *src, uint8_t *dst, size_t srcLength) {
+	if (srcLength < 2) {
+		return dst;
+	}
+
+	memcpy(dst, src, srcLength - 2);
+	
+	return dst;
 }
 
 #endif
--- util/test/checksum.c
+++ util/test/checksum.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
 #include <assert.h>
 
 #include "../checksum.h"
@@ -6,14 +9,109 @@
 void fletcher16Empty(void) {
 	uint8_t data[] = { };
 	size_t len = 0;
-
-	uint16 csum = 0x0000;
+	uint16_t csum = 0x0000;
 	
-	assert(fletcher16(data, len) == 0);
+	assert(fletcher16(data, len) == csum);
+}
+
+void fletcher16Small(void) {
+	uint8_t data[] = { 0x01, 0x02 };
+	size_t len = 2;
+	uint16_t csum = 0x0403;
+
+	assert(fletcher16(data, len) == csum);
+}
+
+void fletcher16Simple(void) {
+	uint8_t data[] = "abcdefgh";
+
+	size_t len1 = 5;
+	uint16_t csum1 = 0xC8F0;
+
+	size_t len2 = 6;
+	uint16_t csum2 = 0x2057;
+
+	size_t len3 = 8;
+	uint16_t csum3 = 0x0627;
+
+
+	assert(fletcher16(data, len1) == csum1);
+	assert(fletcher16(data, len2) == csum2);
+	assert(fletcher16(data, len3) == csum3);
+}
+
+void checkEmpty(void) {
+	uint8_t data[] = { };
+	size_t len = 0;
+
+	assert(check(data, len) == true);
+}
+
+void checkSmall(void) {
+	uint8_t dataGood[] = { 0x01, 0x02, 0xF8, 0x04 };
+	uint8_t dataBad[] = { 0x01, 0x02, 0x00, 0x00 };
+	size_t len = 4;
+
+	assert(check(dataGood, len) == true);
+	assert(check(dataBad, len) == false);
+}
+
+void withCheckBytesSmall(void) {
+	enum {
+		dataLen = 2,
+		bufLen = 255,
+		expectedLen = 4,
+	};
+	
+	uint8_t data[dataLen] = { 0x01, 0x02 };
+	uint8_t buf[bufLen];
+	uint8_t *after = withCheckBytes(data, buf, dataLen);
+	uint8_t expected[expectedLen] = { 0x01, 0x02, 0xF8, 0x04 };
+
+	assert(memcmp(after, expected, expectedLen) == 0);
+}
+
+void withoutCheckBytesEmpty(void) {
+	enum {
+		dataLen = 0,
+		bufLen = 0,
+		expectedLen = 0,
+	};
+	
+	uint8_t data[dataLen] = { };
+	uint8_t buf[bufLen];
+	uint8_t *after = withoutCheckBytes(data, buf, dataLen);
+	uint8_t expected[expectedLen] = { };
+
+	assert(memcmp(after, expected, expectedLen) == 0);
+}
+
+void withoutCheckBytesSmall(void) {
+	enum {
+		dataLen = 4,
+		bufLen = 2,
+		expectedLen = 2,
+	};
+	
+	uint8_t data[dataLen] = { 0x01, 0x02, 0xF8, 0x04 };
+	uint8_t buf[bufLen];
+	uint8_t *after = withCheckBytes(data, buf, dataLen);
+	uint8_t expected[expectedLen] = { 0x01, 0x02 };
+
+	assert(memcmp(after, expected, expectedLen) == 0);
 }
 
 void main(void) {
 	fletcher16Empty();
+	fletcher16Small();
+	fletcher16Simple();
+
+	checkEmpty();
+	checkSmall();
+
+	withCheckBytesSmall();
+	withoutCheckBytesEmpty();
+	withoutCheckBytesSmall();
 
 	printf("All tests passed!");
 }
